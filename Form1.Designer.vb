61bb7025b52da7479cdd3e70225ac70fda6d2110
adminsy5 <piyush@cbigujarat.onmicrosoft.com>
2023-01-22 08:55:14 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.basic = New System.Windows.Forms.TextBox()
+        Me.pf = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.da = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.hra = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ans = New System.Windows.Forms.Label()
+        Me.calc = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(201, 124)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 32)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Basic"
+        '
+        'basic
+        '
+        Me.basic.Location = New System.Drawing.Point(290, 121)
+        Me.basic.Name = "basic"
+        Me.basic.Size = New System.Drawing.Size(147, 39)
+        Me.basic.TabIndex = 1
+        '
+        'pf
+        '
+        Me.pf.Location = New System.Drawing.Point(290, 245)
+        Me.pf.Name = "pf"
+        Me.pf.Size = New System.Drawing.Size(97, 39)
+        Me.pf.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(201, 248)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 32)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Pf(%)"
+        '
+        'da
+        '
+        Me.da.Location = New System.Drawing.Point(290, 183)
+        Me.da.Name = "da"
+        Me.da.Size = New System.Drawing.Size(97, 39)
+        Me.da.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(201, 186)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(77, 32)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Da(%)"
+        '
+        'hra
+        '
+        Me.hra.Location = New System.Drawing.Point(290, 308)
+        Me.hra.Name = "hra"
+        Me.hra.Size = New System.Drawing.Size(97, 39)
+        Me.hra.TabIndex = 9
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(201, 311)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(85, 32)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Hra(%)"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(170, 385)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(154, 32)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Total Salary : "
+        '
+        'ans
+        '
+        Me.ans.AutoSize = True
+        Me.ans.Location = New System.Drawing.Point(330, 385)
+        Me.ans.Name = "ans"
+        Me.ans.Size = New System.Drawing.Size(0, 32)
+        Me.ans.TabIndex = 11
+        '
+        'calc
+        '
+        Me.calc.Location = New System.Drawing.Point(246, 453)
+        Me.calc.Name = "calc"
+        Me.calc.Size = New System.Drawing.Size(150, 46)
+        Me.calc.TabIndex = 12
+        Me.calc.Text = "Calculate"
+        Me.calc.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 32.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(763, 689)
+        Me.Controls.Add(Me.calc)
+        Me.Controls.Add(Me.ans)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.hra)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.da)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.pf)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.basic)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents basic As TextBox
+    Friend WithEvents pf As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents da As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents hra As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ans As Label
+    Friend WithEvents calc As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Dim basicn, dan, pfn, hran As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub calc_Click(sender As Object, e As EventArgs) Handles calc.Click
+        basicn = Val(basic.Text)
+        dan = Val(da.Text)
+        pfn = Val(pf.Text)
+        hran = Val(hra.Text)
+
+        ans.Text = (basicn + (basicn * dan / 100) + (basicn * hran / 100) + (basicn * pfn / 100))
+    End Sub
+End Class
